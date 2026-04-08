f49c6a8b8e536a7a992d93513ac859ac1e25ce85
Amon-Shalem <h9szpnst8c@privaterelay.appleid.com>
2026-04-08 16:29:26 +0800

    update Lesson04

---
+++ Lesson04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace  Lesson04
+{
+    public static class Program
+    {
+        #region 常量
+        
+        //定義編譯時的常量
+        private const string HelloString = "Hello, World!";
+        
+        //定義運行時的常量
+        private static readonly int Value = GetValue();
+        
+        
+        private static int GetValue()   
+        {
+            return 10;
+        }
+        
+        #endregion
+
+        public static void Main(string[] args)
+        {
+            //輸出常量
+            Console.WriteLine(HelloString);
+            Console.WriteLine(Value);
+            
+            //常量不能改變
+            // HelloString = "byebye";
+            // Value = 1;
+        }
+        
+    }
+}
