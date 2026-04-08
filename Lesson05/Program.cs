7e3fde4f60dac3b0f84979dcf402aba930e21940
Amon-Shalem <h9szpnst8c@privaterelay.appleid.com>
2026-04-08 17:04:40 +0800

    Lesson05

---
+++ Lesson05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace  Lesson05
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //算數運算符
+            // var i = 0;
+            // i += 2 * 5 - 3;
+            // Console.WriteLine(i);
+            //邏輯運算符(短路）
+            // var i = 0;
+            // if (i == 0 || i++ >= 1)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            //三元運算符
+            Console.WriteLine(1 > 0 ? 24 : 10);
+            
+        }
+    }
+}
